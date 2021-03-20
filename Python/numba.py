7df86020460ff84e547def76ea60b0e81e03568a
Plydow <plydow@outlook.fr>
2021-03-20 16:03:25 +0100

    initial commit -add numba package in python -add python index -add json package in python

---
+++ Python/numba.py
@@ -0,0 +1,25 @@
+"""
+Nulba est un mobule d'optimisation de python permetant une forte acceleration
+de l'execution du script.
+"""
+#python version
+3.8 and +
+
+#installation
+python -m pip install numba
+
+#docs
+https://numba.readthedocs.io/en/stable/
+
+#utilisation
+ne fonctionne que sur des fonctions (doit pouvoir faire plus mais pas étudié)
+
+from numba import jit
+
+@jit(nopython=True)
+def function(args):
+    'your code here'
+    pass
+
+[w]: la première fois que la fonction est appelée, le temps d'execution est plus
+long.
